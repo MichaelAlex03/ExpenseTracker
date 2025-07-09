bd54c407515e448a1920181cde99dc193b9712d8
Your Name <michaelalex03@outlook.com>
2025-07-09 11:55:08 -0500

    docker no work

--- backend/authService/src/main/java/com/example/backend/BackendApplication.java
+++ backend/authService/src/main/java/com/example/backend/BackendApplication.java
@@ -1,22 +1,31 @@
 package com.example.backend;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import io.github.cdimascio.dotenv.Dotenv;
+ import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class BackendApplication {
 
     public static void main(String[] args) {
 
-        Dotenv dotenv = Dotenv.load();
+          String inDocker = System.getenv("IN_DOCKER");
 
-        System.setProperty("spring.datasource.url", dotenv.get("SPRING_DATASOURCE_URL"));
-        System.setProperty("spring.datasource.username", dotenv.get("SPRING_DATASOURCE_USERNAME"));
-        System.setProperty("spring.datasource.password", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
+          System.out.println("TESTT");
+          System.out.println(System.getenv("SPRING_DATASOURCE_PASSWORD"));
+          System.out.println( System.getProperty("spring.datasource.url"));
 
-        System.setProperty("security.jwt.accessSecret", dotenv.get("ACCESS_TOKEN_SECRET"));
-        System.setProperty("security.jwt.refreshSecret", dotenv.get("REFRESH_TOKEN_SECRET"));
+          if(inDocker == null || !inDocker.equals("true")){
+              Dotenv dotenv = Dotenv.load();
+
+              System.setProperty("spring.datasource.url", dotenv.get("SPRING_DATASOURCE_URL"));
+              System.setProperty("spring.datasource.username", dotenv.get("SPRING_DATASOURCE_USERNAME"));
+              System.setProperty("spring.datasource.password", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
+
+              System.setProperty("security.jwt.accessSecret", dotenv.get("ACCESS_TOKEN_SECRET"));
+              System.setProperty("security.jwt.refreshSecret", dotenv.get("REFRESH_TOKEN_SECRET"));
+          }
 
         SpringApplication.run(BackendApplication.class, args);
     }
