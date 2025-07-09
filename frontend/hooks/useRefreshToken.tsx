abb9528c6b24cabc708c7f597ad295ffec966899
Your Name <michaelalex03@outlook.com>
2025-07-08 19:00:13 -0500

    refresh hook

--- frontend/hooks/useRefreshToken.tsx
+++ frontend/hooks/useRefreshToken.tsx
@@ -1,8 +1,30 @@
 import React from "react";
 import useAuth from "./useAuth";
+import axios from "../api/axios";
 
 const useRefreshToken = () => {
   const { auth, setAuth } = useAuth();
+
+  const refresh = async () => {
+    try {
+      const response = await axios.get("/auth/refresh", {
+        withCredentials: true,
+      });
+
+      setAuth({
+        ...auth,
+        email: response.data.email,
+        userId: response.data.userId,
+        accessToken: response.data.accessToken,
+      });
+
+      return response.data.accessToken;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return refresh;
 };
 
 export default useRefreshToken;
