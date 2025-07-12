2f6198519fbb668f3ba98797400c85346af553fc
Your Name <michaelalex03@outlook.com>
2025-07-12 01:31:58 -0500

    changed docker to ipv6 to work with supabase

--- backend/authService/src/main/java/com/example/backend/BackendApplication.java
+++ backend/authService/src/main/java/com/example/backend/BackendApplication.java
@@ -2,8 +2,10 @@ package com.example.backend;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
  import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 public class BackendApplication {
