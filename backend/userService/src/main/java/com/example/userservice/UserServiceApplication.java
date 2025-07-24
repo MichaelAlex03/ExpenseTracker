7434b4690e6dce017874bfc14e50dbd09d2e7999
Your Name <michaelalex03@outlook.com>
2025-07-24 13:55:05 -0500

    commit

--- backend/userService/src/main/java/com/example/userservice/UserServiceApplication.java
+++ backend/userService/src/main/java/com/example/userservice/UserServiceApplication.java
@@ -2,11 +2,24 @@ package com.example.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
+
+		String inDocker = System.getenv("IN_DOCKER");
+
+		if(inDocker == null || !inDocker.equals("true")){
+			Dotenv dotenv = Dotenv.load();
+
+			System.setProperty("spring.datasource.url", dotenv.get("SPRING_DATASOURCE_URL"));
+			System.setProperty("spring.datasource.username", dotenv.get("SPRING_DATASOURCE_USERNAME"));
+			System.setProperty("spring.datasource.password", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
+
+		}
+
 		SpringApplication.run(UserServiceApplication.class, args);
 	}
 
